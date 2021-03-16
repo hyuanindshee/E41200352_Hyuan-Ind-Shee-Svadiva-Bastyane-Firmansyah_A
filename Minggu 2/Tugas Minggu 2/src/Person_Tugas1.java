0e64bf722072425523b3ea59897771d8b145401c
hyuanindshee <hyuanind@gmail.com>
2021-03-16 19:10:54 +0700

    Minggu 2

---
+++ Minggu 2/Tugas Minggu 2/src/Person_Tugas1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author HP
+ */
+public class Person_Tugas1 {
+    
+    public static void main(String[] args) {
+        
+        String fName = "Hyuan Ind Shee ";
+        String IName = "Svadiva Bastyane Firmansyah ";
+        int stuid = 41200352;
+        String stuStatus = "Active";
+        
+        System.out.println("Student Name: " + fName +""+ IName);
+        System.out.println("Student ID: " + stuid);
+        System.out.println("Student Status: " + stuStatus);
+    }  
+}
---
+++ Minggu 2/Tugas Minggu 2/src/pendeklarasianvariable_Tugas3.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author HP
+ */
+public class pendeklarasianvariable_Tugas3 {
+    public static void main(String[] args) {
+        
+        // pendeklarasian yang benar
+        boolean gameOver=false;
+        int students=50,classes=3;
+        double sales_tax;
+        short numberl;
+        
+        // pendeklarasian yang salah
+        int 2beOrNot2be;
+        float price index;
+        double lastYear'sPrice;
+        long class;
+    }   
+}
